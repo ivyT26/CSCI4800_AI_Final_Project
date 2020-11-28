0f3604487d08092b4f209b761513e7fb61727e0b
shogun87 <45832125+shogun87@users.noreply.github.com>
2020-11-28 12:20:13 -0700

    Update reward_function.py
    Version 4-1. Modifying progress parameter to give a smaller percentage of reward than we gave in V4. We will also give a larger penalty if the deepracer gets close to going off track, or does go off track.

--- reward_function.py
+++ reward_function.py
@@ -10,6 +10,7 @@ def reward_function(params):
     is_left_of_center = params['is_left_of_center']
     progress = params['progress']
     
+    
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
@@ -23,23 +24,24 @@ def reward_function(params):
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
-        reward = 1e-3  # likely crashed/ close to off track
+        reward = 1e-6  # likely crashed/ close to off track
       
-    # Check if Deepracer is left of center and turning left, give penalty  
+    # Check if Deepracer is left of center and turning left, give penality  
     if is_left_of_center == True and steering_angle > 0:
         reward *= 0.8
     # Check if Deepracer is left of center and turning right, give reward
     elif is_left_of_center == True and steering_angle < 0:
         reward *= 1.4
-    # Check if Deepracer is right of center and turning right, give penalty
+        # Check if Deepracer is right of center and turning right, give penality
     elif is_left_of_center == False and steering_angle < 0:
         reward *= 0.8
     # Check if Deepracer is right of center and turning left, give reward
     elif is_left_of_center == False and steering_angle > 0:
         reward *= 1.4
-    
-    # Reward the deepracer based on progess after 25% completion.
+        
+    # Reward the deepracer based on progress made after completing 25% of the track.
     if progress >= 25:
-        reward = reward * (1 + (progress / 100))
+        reward = reward * (1 + ((progress / 4) / 100))
+    
     
     return float(reward)
