1914afbecc8f924886a32a9ebae3823a77a80793
shogun87 <45832125+shogun87@users.noreply.github.com>
2020-11-28 15:12:16 -0700

    Update reward_function.py
    Version 6. Cloned V3 using a modified version of V4-2's code. We wanted to implement higher reward initializations (i.e., 10.0, 5.0, 1.0, 1e-3) instead of (1.0, 0.5, 0.1, and 1e-6). We also wanted to try adding and subtracting rewards instead of using multiplication to increase or decrease rewards by a percentage. We will also have a check at the end of the reward function to see if the reward is negative, and if it is we will change the reward to a small value such as 1e-6. We wanted to start these changes from a model that had a successful Reward graph such as V3.

--- reward_function.py
+++ reward_function.py
@@ -1,4 +1,13 @@
-# V5
+# V6
+
+'''
+Cloned V3 using a modified version of V4-2's code. We wanted to implement higher reward initializations 
+(i.e., 10.0, 5.0, 1.0, 1e-3) instead of (1.0, 0.5, 0.1, and 1e-6). We also wanted to try adding and subtracting rewards
+instead of using multiplication to increase or decrease rewards by a percentage. We will also have a check at the end
+of the reward function to see if the reward is negative, and if it is we will change the reward to a small value such
+as 1e-6. We wanted to start these changes from a model that had a successful Reward graph such as V3.
+'''
+
 def reward_function(params):
     '''
     Example of rewarding the agent to follow center line
@@ -19,30 +28,33 @@ def reward_function(params):
     
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1.0
+        reward = 10.0
     elif distance_from_center <= marker_2:
-        reward = 0.5
+        reward = 5.0
     elif distance_from_center <= marker_3:
-        reward = 0.1
+        reward = 1.0
     else:
-        reward = 1e-6  # likely crashed/ close to off track
+        reward = 1e-3  # likely crashed/ close to off track
       
     # Check if Deepracer is left of center and turning left, give penalty  
     if is_left_of_center == True and steering_angle > 0:
-        reward *= 0.9
+        reward -= 1.0
     # Check if Deepracer is left of center and turning right, give reward
     elif is_left_of_center == True and steering_angle < 0:
-        reward *= 1.1
+        reward += 1.0
         # Check if Deepracer is right of center and turning right, give penalty
     elif is_left_of_center == False and steering_angle < 0:
-        reward *= 0.9
+        reward -= 1.0
     # Check if Deepracer is right of center and turning left, give reward
     elif is_left_of_center == False and steering_angle > 0:
-        reward *= 1.1
+        reward += 1.0
         
     # Reward the deepracer based on progress made after completing 25% of the track.
     if progress >= 25:
-        reward = reward * (1 + ((progress / 4) / 1000)) # 25% -> 1.0062 // 60% -> 1.015
+        reward = reward + (progress / 100) # 25% -> reward + 0.25 // 60% -> reward + 0.60
     
+    # Check if reward is 0 or below and if so make reward a small decimal value    
+    if reward <= 0.0:
+        reward = 1e-6
     
     return float(reward)
