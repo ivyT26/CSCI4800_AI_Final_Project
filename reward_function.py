591479df9953b67943f6245ccdf3ecad45a84961
shogun87 <45832125+shogun87@users.noreply.github.com>
2020-11-28 17:40:55 -0700

    Update reward_function.py
    Cloning V2, adding all_wheels_on_track, and progress, and speed.

--- reward_function.py
+++ reward_function.py
@@ -1,18 +1,13 @@
-# V7
+# V8
 
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
     
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    steering_angle = params['steering_angle']
-    is_left_of_center = params['is_left_of_center']
     progress = params['progress']
     speed = params['speed']
-    
+    all_wheels_on_track = params['all_wheels_on_track']
     
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
@@ -28,19 +23,6 @@ def reward_function(params):
         reward = 1.0
     else:
         reward = 1e-3  # likely crashed/ close to off track
-      
-    # Check if Deepracer is left of center and turning left, give penalty  
-    if is_left_of_center == True and steering_angle > 0:
-        reward -= 1.0
-        # Check if Deepracer is left of center and turning right, give reward
-    elif is_left_of_center == True and steering_angle < 0:
-        reward += 1.0
-        # Check if Deepracer is right of center and turning right, give penalty
-    elif is_left_of_center == False and steering_angle < 0:
-        reward -= 1.0
-    # Check if Deepracer is right of center and turning left, give reward
-    elif is_left_of_center == False and steering_angle > 0:
-        reward += 1.0
         
     # Reward the deepracer based on progress made after completing 25% of the track.
     if progress >= 25:
@@ -51,6 +33,12 @@ def reward_function(params):
         reward += 0.5
     else:
         reward -= 0.5
+        
+    # Check if all wheels are on track, if they are reward deepracer, if not give penalty
+    if all_wheels_on_track:
+        reward += 1.0
+    else:
+        reward -= 2.0
     
     # Check if reward is 0 or below and if so make reward a small decimal value    
     if reward <= 0.0:
